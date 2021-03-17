255b164f983d1111292fb8f285210a240b69be82
Andrea <andreamarongiu05@gmail.com>
2021-03-17 15:57:55 +0100

    input con aggiungi lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,25 @@ $(document).ready(function() {
 // Rendiamo il tutto gradevole alla vista.
 
 // Seleziono il pulsante add
-$('.titlelist #add').click(function() {
-  var inputAdd = $('input#addList').val();
 
+// Seleziono il pulsante add
+$('.titlelist #add').click(function() {
+  // seleziono input add
+  var inputAdd = $('#addlist').val();
+  // imposto il valore a 0
+  $('#addlist').val('');
+  // clono template
+  var template = $('.template li').clone();
+  // gli salvo il valore dell'input
+  template.text(inputAdd);
+
+  // gli aggiungo il valore di input add
+// Seleziono la mia lista
+var lista = $('.todolista ul');
+// appendo template nella lista
+lista.append(template)
 
 });
-
-
-
 }); // fine ready
 // imposto data e ora
 
