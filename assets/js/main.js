9138b056a07fd4723bca8e34f8db7c5b1f7224a7
Andrea <andreamarongiu05@gmail.com>
2021-03-17 17:02:21 +0100

    inserimento pulsante edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,37 @@
 $(document).ready(function() {
-//   Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
-// Rendiamo il tutto gradevole alla vista.
+  //   Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
+  // Rendiamo il tutto gradevole alla vista.
 
-// Seleziono il pulsante add
+  // Seleziono il pulsante add
+  $('.titlelist #add').click(function() {
+    // seleziono input add
+    var inputAdd = $('#addlist').val();
+    // imposto il valore a 0
+    $('#addlist').val('');
+    // clono template
+    var template = $('.template li').clone();
+    // gli salvo il valore dell'input
+    template.prepend(inputAdd);
+    // gli aggiungo il valore di input add
+    // Seleziono la mia lista
+    var list = $('.todolista ul');
+    // appendo template nella lista
+    list.append(template);
 
-// Seleziono il pulsante add
-$('.titlelist #add').click(function() {
-  // seleziono input add
-  var inputAdd = $('#addlist').val();
-  // imposto il valore a 0
-  $('#addlist').val('');
-  // clono template
-  var template = $('.template li').clone();
-  // gli salvo il valore dell'input
-  template.prepend(inputAdd);
-  // gli aggiungo il valore di input add
-// Seleziono la mia lista
-var list = $('.todolista ul');
-// appendo template nella lista
-list.append(template);
-
-  // Ora seleziono il bottone rimuovi
-  var remove = $('.remove');
-  // gli seleziono il click
-  remove.click(function(event) {
-    // al click rimuovo il genitore (li)
-    $(this).parent().remove();
+    // Ora seleziono il bottone rimuovi
+    var remove = $('.remove');
+    // gli seleziono il click
+    remove.click(function(event) {
+      // al click rimuovo il genitore (li)
+      $(this).parent().remove();
+    });
   });
-});
 }); // fine ready
+
 // imposto data e ora
 
-// setInterval(function () {
-//   var data = new Date().toLocaleString();
-//   $('span#currentDate').text(data);
-//   //imposto un intervallo
-// },1000)
+setInterval(function () {
+  var data = new Date().toLocaleString();
+  $('.currentDate').text(data);
+  //imposto un intervallo
+},1000)
